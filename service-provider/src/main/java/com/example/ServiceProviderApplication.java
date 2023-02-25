bc5906b03f19e9bc3ccf4a9e7f42548a0c9a0d02
root <lizongzai@gmail.com>
2023-02-26 00:14:48 +0800

    v3 consul服务中心环境搭建

--- service-provider/src/main/java/com/example/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/example/ServiceProviderApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class ServiceProviderApplication {
-
   public static void main(String[] args) {
-
     SpringApplication.run(ServiceProviderApplication.class, args);
   }
 }
